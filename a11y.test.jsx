fba4ed6617e3bfdf781a4e749c67983bcd7f796d
ngongang <70435295+bakadja@users.noreply.github.com>
2025-08-27 14:08:31 +0200

    Remove accessibility test for meme image from a11y.test.jsx

--- a11y.test.jsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import { expect, test } from "vitest";
-import App from "./src/App";
-
-test(`renders the meme image with accessible alt text "One Does Not Simply"`, () => {
-    render(<App/>)
-    expect(screen.getByAltText("One Does Not Simply")).toBeInTheDocument()
-})
